ef4f2307ca6cb0658ae47bfc9004924e67139fad
Dany Atanov <Dany Atanov>
2025-02-04 15:30:01 +0300

    [+] adding js magic

--- index.php
+++ index.php
@@ -1,32 +1,60 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1); ?>
+<html lang="ru">
 
-interface Template
-{
-	public function get();
-}
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<!-- use xlsx.mini.min.js from version 0.20.3 -->
+	<script lang="javascript" src="https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.mini.min.js"></script>
+	<script defer src='/index.js'></script>
+</head>
 
-class InvoiceTemplate implements Template
-{
-	public function get()
+<body>
+	<?php
+
+	interface Template
 	{
-		if (file_exists('./template.php')) {
-			return require('./template.php');
-		} else {
-			echo 'Файл не найден';
+		public function get();
+	}
+
+	class InvoiceTemplate implements Template
+	{
+		public function get()
+		{
+			if (file_exists('./template.php')) {
+				return require('./template.php');
+			} else {
+				echo 'Файл не найден';
+			}
 		}
 	}
-}
 
-class GenerateInvoice
-{
-	public function generate(Template $template)
+	class GenerateInvoice
 	{
-		echo $template->get();
+		public function generate(Template $template)
+		{
+			echo $template->get();
+		}
 	}
-}
 
 
-$g1 = new GenerateInvoice();
-$g1->generate(new InvoiceTemplate());
+	$g1 = new GenerateInvoice();
+	$g1->generate(new InvoiceTemplate());
+	?>
+
+<button id="sheetjsexport"><b>Export as XLSX</b></button>
+
+<script>
+document.getElementById("sheetjsexport").addEventListener('click', function() {
+  /* Create worksheet from HTML DOM TABLE */
+  var wb = XLSX.utils.table_to_book(document.getElementById("TableToExport"));
+  /* Export to file (start a download) */
+  XLSX.writeFile(wb, "SheetJSTable.xlsx");
+});
+</script>
+</body>
+
+</html>
